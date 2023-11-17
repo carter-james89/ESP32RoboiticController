51fafbd4c595890c8009e066b8626effc6ff7357
Carter <carter.egan@gmail.com>
2023-11-17 09:17:43 -0600

    Update QuadrupedConfiguration.cpp

--- ESP32RoboticController/QuadrupedConfiguration.cpp
+++ ESP32RoboticController/QuadrupedConfiguration.cpp
@@ -12,11 +12,11 @@ QuadrupedConfiguration::~QuadrupedConfiguration() {
     // Destructor body (if needed)
 }
 
-// std::vector<QuadrupedLimbData> QuadrupedConfiguration::GetLimbData() {
-//     std::vector<QuadrupedLimbData> data;
-//     // Fill 'data' with the appropriate limb data
-//     return data;
-// }
+std::vector<QuadrupedLimbData> QuadrupedConfiguration::GetLimbData() {
+    std::vector<QuadrupedLimbData> data;
+    // Fill 'data' with the appropriate limb data
+    return data;
+}
 
 
 // Note: No implementation for pure virtual functions is needed here
